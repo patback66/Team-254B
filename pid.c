52437348a45c9fa9e6368667c512373c84ba8d70
patback66 <maskoken@gmail.com>
2012-09-27 15:17:29 -0700

    Added additional file for pid

---
+++ pid.c
@@ -0,0 +1,12 @@
+#pragma config(Motor,  port1,           fLeft,         tmotorVex393, openLoop)
+#pragma config(Motor,  port2,           bLeft,         tmotorVex393, openLoop)
+#pragma config(Motor,  port3,           atLeft,        tmotorVex269, openLoop)
+#pragma config(Motor,  port4,           abLeft,        tmotorVex269, openLoop, reversed)
+#pragma config(Motor,  port5,           rollersLeft,   tmotorVex269, openLoop)
+#pragma config(Motor,  port6,           fRight,        tmotorVex393, openLoop, reversed)
+#pragma config(Motor,  port7,           bRight,        tmotorVex393, openLoop, reversed)
+#pragma config(Motor,  port8,           atRight,       tmotorVex269, openLoop)
+#pragma config(Motor,  port9,           abRight,       tmotorVex269, openLoop, reversed)
+#pragma config(Motor,  port10,          rollersRight,  tmotorVex269, openLoop, reversed)
+
+//PID file
